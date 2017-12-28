acc1e20a6c6236561137c436d803fada950bb98f
1weidong <649149488@qq.com>
2017-12-28 14:54:01 +0800

    提交文件

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <router-view
+      class="view"
+      keep-alive
+      transition
+      transition-mode="out-in">
+    </router-view>
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style>
+  @import "./style/scss/style.scss";
+</style>
